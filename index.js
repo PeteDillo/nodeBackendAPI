d68d12e21adfd30177395d0f14ded150127b1443
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2022-04-09 19:19:39 -0500

    made first end point

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+//imports
 const express = require("express");
 const app = express();
 
-app.use(express.json);
 
+//Middleware
+app.use(express.json());
+
+//Endpoints
+//http://localhost:5005
+//http://localhost:5005
+app.get("/api/products", (req, res)=>{
+    console.log(req.headers);
+res.send("response!");
+});
+
+//Server
 const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => {
