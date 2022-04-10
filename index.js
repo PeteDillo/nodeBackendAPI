716d3e8621cdc7859ad93a2872340c03a874c751
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2022-04-10 01:19:16 -0500

    added 2 get request a post request and a delete request.

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 //imports
 const express = require("express");
 const app = express();
-
+const repoContext = require("./repository/repository-wrapper");
 
 //Middleware
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 //Endpoints
-//http://localhost:5005
-//http://localhost:5005
-app.get("/api/products", (req, res)=>{
-    console.log(req.headers);
-res.send("response!");
+//GET products
+//http://localhost:5005/api/products
+app.get("/api/products", (req, res) => {
+  const products = repoContext.products.findAllProducts();
+  console.log(req.headers);
+  return res.send(products);
 });
+
+//GET products by ID
+//http://localhost:5005/api/products/:id
+app.get("/api/products/:id", (req, res) => {
+  const id = req.params.id;
+  const product = repoContext.products.findProductById(id);
+  return res.send(product);
+});
+
+//POST new product
+//http://localhost:5005/api/products
+app.post("/api/products", (req, res) => {
+  const newProduct = req.body;
+  const addedProduct = repoContext.products.createProduct(newProduct);
+  return res.status(201).send(addedProduct);
+});
+
+//DELETE products by ID
+//http://localhost:5005/api/products/:id
+app.delete("/api/products/:id", (req, res) => {
+    const id = req.params.id;
+    const product = repoContext.products.deleteProduct(id)
+    return res.send(product);
+  });
 
 //Server
 const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => {
-    console.log(`Server is running! on Port: ${PORT}`);
+  console.log(`Server is running! on Port: ${PORT}`);
 });
